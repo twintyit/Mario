6e38f9dc4ad4d15d51e3aa2298ce50a2b62039b9
Kamyshov Ruslan <kamyshov02u@gmail.com>
2024-01-03 20:14:57 +0100

    animations

--- Assets/Scripts/Move1.cs
+++ Assets/Scripts/Move1.cs
@@ -8,10 +8,12 @@ public class Move1 : MonoBehaviour
     public float speed = 5f;    
     public float jumpForce = 10f;    
     private Rigidbody2D rb;
-
+    public Vector2 moveVector;
+    public Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +28,9 @@ public class Move1 : MonoBehaviour
 
     void Move()
     {
+        moveVector.x = Input.GetAxis("Horizontal");
+        anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
+
         float horizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(horizontal, 0f);
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
